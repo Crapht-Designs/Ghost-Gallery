5d30d66f0cc3ba1d7cadbd236e0e765ba098ec91
Chouaten Yann <167680678+ChouatenY@users.noreply.github.com>
2025-11-18 16:50:15 +0100

    verel deploy update 7

--- src/data.js
+++ src/data.js
@@ -1,151 +1,151 @@
 export const projects = [
     {
         title: "The Fall Front",
-        image: "/dark-hoodie-front.png",
+        image: "/images/dark-hoodie-front.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "The Fall Front",
-        image: "/dark-hoodie-back.png",
+        image: "/images/dark-hoodie-back.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "Skull hoodie Front",
-        image: "/red-skull.png",
+        image: "/images/red-skull.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "Skull hoodie Front",
-        image: "/red-skull.png",
+        image: "/images/red-skull.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "Red Skull hoodie Front",
-        image: "/red-skull.png",
+        image: "/images/red-skull.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "Wild west hoodie Front",
-        image: "/wild-west-front.png",
+        image: "/images/wild-west-front.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "Wild west hoodie Back",
-        image: "/wild-west-back.png",
+        image: "/images/wild-west-back.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "Angel hoodie Front",
-        image: "/angel-front-hoodie.png",
+        image: "/images/angel-front-hoodie.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "Angel hoodie Back",
-        image: "/angel-back-hoodie.png",
+        image: "/images/angel-back-hoodie.png",
         year: 2025,
         href: "/sample-projects"
     },
     {
         title: "80 sweater Front",
-        image: "/sweater-front.png",
+        image: "/images/sweater-front.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "80 sweater Back",
-        image: "/sweater-back.png",
+        image: "/images/sweater-back.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "80 Red sweater Front",
-        image: "/sweater-red-front.png",
+        image: "/images/sweater-red-front.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "80 Red sweater Back",
-        image: "/sweater-red-back.png",
+        image: "/images/sweater-red-back.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Feeling hoodie Front",
-        image: "/flower-hoodie-front.png",
+        image: "/images/flower-hoodie-front.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Feeling hoodie Back",
-        image: "/flower-hoodie-back.png",
+        image: "/images/flower-hoodie-back.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Fairy hoodie Front",
-        image: "/hoodie.png",
+        image: "/images/hoodie.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Fairy hoodie Back",
-        image: "/hoodie1.png",
+        image: "/images/hoodie1.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Fangs hoodie Front",
-        image: "/teeth-hoodie-front.png",
+        image: "/images/teeth-hoodie-front.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Fangs hoodie Back",
-        image: "/teeth-hoodie-back.png",
+        image: "/images/teeth-hoodie-back.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Bird Shirt Front",
-        image: "/shirt.png",
+        image: "/images/shirt.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Punk sweater Front",
-        image: "/punk.png",
+        image: "/images/punk.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Butterfly hoodie Front",
-        image: "/grey-hoodie-front.png",
+        image: "/images/grey-hoodie-front.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Butterfly hoodie Back",
-        image: "/grey-hoodie-back.png",
+        image: "/images/grey-hoodie-back.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Brown hoodie Front",
-        image: "/brown-hoodie-front.png",
+        image: "/images/brown-hoodie-front.png",
         year: 2024,
         href: "/sample-projects"
     },
     {
         title: "Punk gray sweater Front",
-        image: "/punk2.png",
+        image: "/images/punk2.png",
         year: 2024,
         href: "/sample-projects"
     },
